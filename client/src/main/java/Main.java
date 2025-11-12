3b7e57bd551b60a00174ac1b93579f92e47fbf8e
Ianflowers <mazing.pollo@gmail.com>
2025-11-11 23:06:36 -0700

    Base prelogin running on main--transferring to PreloginUI with real authentication.

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,58 @@
+
 import chess.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("¡ Welcome to 240 Chess! Type Help to get started. ¿");
+
+        boolean quit = false;
+
+        while (!quit) {
+            System.out.print("[LOGGED_OUT] >>> ");
+            var line = scanner.nextLine().trim();
+            var parts = line.split("\\s+");
+            var cmd = parts[0].toLowerCase();
+
+            switch (cmd) {
+                case "help":
+                    System.out.println("""
+                                register <USERNAME> <PASSWORD> <EMAIL> - to create an account
+                                login <USERNAME> <PASSWORD> - to play chess
+                                quit - exit the program
+                                help - list possible commands
+                                """);
+                    break;
+
+                case "quit":
+                    quit = true;
+                    break;
+
+                case "register":
+                    if (parts.length != 4) {
+                        System.out.println("Usage: register <USERNAME> <PASSWORD> <EMAIL>");
+                        break;
+                    }
+                    System.out.println("Registering account...");
+                    break;
+
+                case "login":
+                    if (parts.length != 3) {
+                        System.out.println("Usage: login <USERNAME> <PASSWORD>");
+                        break;
+                    }
+                    System.out.println("Searching for account...");
+                    break;
+
+                default:
+                    System.out.println("Unknown command. Type 'help' for options.");
+                    break;
+
+            }
+
+        }
+
+        System.out.println("Goodbye!");
     }
 }
