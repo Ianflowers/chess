783e772ad931c3220133a6cfb5903365e1e31133
Ianflowers <mazing.pollo@gmail.com>
2025-12-03 10:37:39 -0700

    Added websocket endpoints in Server.java and implemented them in WSHandler.java

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -2,7 +2,6 @@ package server;
 
 import com.google.gson.Gson;
 import io.javalin.*;
-
 import dataaccess.*;
 import result.*;
 import service.*;
@@ -29,6 +28,7 @@ public class Server {
     private final AuthHandler authHandler = new AuthHandler(authService, gson);
     private final GameHandler gameHandler = new GameHandler(gameService, gson);
     private final ClearHandler clearHandler = new ClearHandler(clearService, gson);
+    private final WSHandler wsHandler = new WSHandler(gson);
 
     public Server() {
         try {
@@ -48,27 +48,17 @@ public class Server {
     }
 
     private void registerEndpoints() {
+        // HTTP Endpoints
+        javalin.post("/user", userHandler.registerUser);    // User Registration - POST /user
+        javalin.post("/session", authHandler.loginUser);    // User Login - POST /user
+        javalin.delete("/session", authHandler.logoutUser); // User Logout - DELETE /session
+        javalin.get("/game", gameHandler.listGames);        // List Games - GET /game
+        javalin.post("/game", gameHandler.createGame);      // Create Game - POST /game
+        javalin.put("/game", gameHandler.joinGame);         // Join Game - PUT /game
+        javalin.delete("/db", clearHandler.clearAll);       // Clear DB - DELETE /db
 
-        // User Registration - POST /user
-        javalin.post("/user", userHandler.registerUser);
-
-        // User Login - POST /user
-        javalin.post("/session", authHandler.loginUser);
-
-        // User Logout - DELETE /session
-        javalin.delete("/session", authHandler.logoutUser);
-
-        // List Games - GET /game
-        javalin.get("/game", gameHandler.listGames);
-
-        // Create Game - POST /game
-        javalin.post("/game", gameHandler.createGame);
-
-        // Join Game - PUT /game
-        javalin.put("/game", gameHandler.joinGame);
-
-        // Clear DB - DELETE /db
-        javalin.delete("/db", clearHandler.clearAll);
+        // WebSocket Endpoint
+        javalin.ws("/ws", wsHandler.ws);
     }
 
     private void registerExceptionHandlers() {
