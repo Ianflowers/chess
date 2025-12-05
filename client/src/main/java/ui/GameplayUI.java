d88b4d86805ef833249fc323bfdcbb7cc4cc2ee6
Ianflowers <mazing.pollo@gmail.com>
2025-12-05 10:35:54 -0700

    WebSocketListener set up, GameplayUI added for game interface handling

---
+++ client/src/main/java/ui/GameplayUI.java
@@ -0,0 +1,16 @@
+package ui;
+
+import model.GameData;
+
+public class GameplayUI {
+
+
+    public void redrawBoard(GameData game) { }
+
+    public void showNotification(String message) { }
+
+    public void showError(String errorMessage) { }
+
+    public void onDisconnect() { }
+
+}
--- client/src/main/java/websocket/WebSocketListener.java
+++ client/src/main/java/websocket/WebSocketListener.java
@@ -1,7 +1,51 @@
 package websocket;
 
+import com.google.gson.Gson;
+import ui.GameplayUI;
+import websocket.messages.*;
 import java.net.http.WebSocket;
+import java.util.concurrent.CompletionStage;
 
 public class WebSocketListener implements WebSocket.Listener {
+
+    private final Gson gson = new Gson();
+    private final GameplayUI ui;
+
+    public WebSocketListener(GameplayUI ui) { this.ui = ui; }
+
+    @Override
+    public void onOpen(WebSocket webSocket) { webSocket.request(1); }
+
+    @Override
+    public CompletionStage<?> onText(WebSocket webSocket, CharSequence data, boolean last) {
+        String json = data.toString();
+        ServerMessage base = gson.fromJson(json, ServerMessage.class);
+
+        switch (base.getServerMessageType()) {
+            case LOAD_GAME -> {
+                LoadGameMessage msg = gson.fromJson(json, LoadGameMessage.class);
+                ui.redrawBoard(msg.getGame());
+            }
+
+            case NOTIFICATION -> {
+                NotificationMessage msg = gson.fromJson(json, NotificationMessage.class);
+                ui.showNotification(msg.getMessage());
+            }
+
+            case ERROR -> {
+                ErrorMessage msg = gson.fromJson(json, ErrorMessage.class);
+                ui.showError(msg.getErrorMessage());
+            }
+        }
+
+        webSocket.request(1);
+        return null;
+    }
+
+    @Override
+    public CompletionStage<?> onClose(WebSocket webSocket, int statusCode, String reason) {
+        ui.onDisconnect();
+        return null;
+    }
 
 }
